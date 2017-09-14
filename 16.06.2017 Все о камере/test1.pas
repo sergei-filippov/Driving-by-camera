c2737b719bb91fa9b64d034d959c1a2f210d15a9
sergei <sergei-filippov2001@protonmail.com>
2017-09-14 19:55:20 +0300

    pascal

---
+++ 16.06.2017 Все о камере/test1.pas
@@ -0,0 +1,16 @@
+uses GraphABC;
+var angl:real:=-25;
+var v:integer:=1;
+var w:integer:=40;
+var distance:integer:=50;
+
+begin
+while(true) do
+begin
+clearwindow;
+lockdrawing;
+w:=Window.Width;
+rectangle(20,50,w-20,60);
+redraw;
+end;
+end.

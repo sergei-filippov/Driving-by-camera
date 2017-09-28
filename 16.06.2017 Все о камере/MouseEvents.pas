097312cb1df69de86e95e1fb23d67f51abc2dac7
sergei <sergei-filippov2001@protonmail.com>
2017-09-28 20:01:55 +0300

    mouse

---
+++ 16.06.2017 Все о камере/MouseEvents.pas
@@ -0,0 +1,28 @@
+﻿// Иллюстрация обработки событий мыши
+uses GraphABC;
+var xn,yn:integer;
+procedure MouseDown(x,y,mb: integer);
+begin
+
+  MoveTo(x,y);
+  setpixel(x,y,clred);
+   
+end;
+
+procedure MouseMove(x,y,mb: integer);
+begin
+  if mb=1 then LineTo(x,y);
+  xn :=x;
+yn :=y;
+ println(xn,yn);
+end;
+
+begin
+setconsoleio;
+  // Привязка обработчиков к событиям
+  //writeln(OnMouseDown);
+  OnMouseDown := MouseDown;
+  println(xn,yn);
+  OnMouseMove := MouseMove;
+end.
+

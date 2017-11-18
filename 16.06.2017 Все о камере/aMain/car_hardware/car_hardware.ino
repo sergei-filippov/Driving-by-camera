cee9990f5a2526b97e64bf800e3df65f2c3267c0
sergei-filippov <sergei-filippov2001@yandex.ru>
2017-11-18 22:38:00 +0300

    code is more gentle

--- 16.06.2017 Все о камере/aMain/car_hardware/car_hardware.ino
+++ 16.06.2017 Все о камере/aMain/car_hardware/car_hardware.ino
@@ -4,7 +4,6 @@ Servo servo;
 
 
 //------------------------------//irda codes
-//--------------------------------------------------// i'm really not sure in the this defining
 #define red 0
 #define red_yellow  1
 #define yellow  4
@@ -14,133 +13,117 @@ Servo servo;
 #define stop_sign 6
 //------------------------------//
 
-int angle, speed1, irda, d1, d2, d3, distanceEdge, speedBeforCrossing, slowSpeed;
+int angle, speed1, irda, d1, d2, d3, distanceEdge, speedBeforCrossing, slowSpeed, incomingByte;;
 
 bool stopline;
 
 
+//----------------------//motor driver - pins
 const int inaPin = 26;
 const int inbPin = 27;
 const int pwm = 3;
 const int diagaPin = 24;
 const int diagbPin = 25;
-int incomingByte;
+
 void setup() {
 
   //-----------------//distance detectors
   pinMode(A6, INPUT);
   pinMode(A7, INPUT);
   pinMode(A8, INPUT);
-  //----------------//
-  servo.attach(20);    //serva 55-125
-  delay(1000);
-  pinMode(inaPin, OUTPUT);   //5
-  pinMode(inbPin, OUTPUT);   //4
-  pinMode(pwm, OUTPUT);   //3
-  pinMode(diagaPin, INPUT);  //7
-  pinMode(diagbPin, INPUT);  //8
+
+  servo.attach(20);   //possible values 55-125
+  delay(1000);        // doesn't work without
+
+  //-------------------------------------------//motor driver
+  pinMode(inaPin, OUTPUT);   //num 5 on driver
+  pinMode(inbPin, OUTPUT);   //num 4 on driver
+  pinMode(pwm, OUTPUT);      //num 3 on driver
+  pinMode(diagaPin, INPUT);  //num 7 on driver
+  pinMode(diagbPin, INPUT);  //num 8 on driver
 
 
-  //pinMode(40, INPUT);     //stop-line existence
 
-
-
-  digitalWrite(inaPin, LOW); // for motors
+  //-------------------------//motors 
+  digitalWrite(inaPin, LOW);
   digitalWrite(inbPin, HIGH);
 
-  Serial.begin(9600);
-  Serial1.begin(9600); //seeeduino angle
+
+  Serial.begin(9600);    //pc connection
+  Serial1.begin(9600);   //seeeduino angle
   Serial2.begin(115200); //irda
+  Serial3.begin(115200); //seeeduino stop line
 
-  Serial3.begin(9600); //seeeduino stop line
-  speed1 = 40;
 
-  Serial3.begin(115200); //stopline
-  speed1 = 70;
 
-  distanceEdge = 20;
-  slowSpeed = 50;
+  speed1 = 70;         // usual speed
+  distanceEdge = 20;   // max distance without barrier
+  slowSpeed = 50;      // speed when...
 }
 
 void loop() {
-  if(Serial2.available()){
-    irda = Serial2.read();
-    if(irda == 0 || irda == 1 || irda == 4){
-      analogWrite(pwm,0);
-    }
-  }
-  analogWrite(pwm, speed1);
-  //----------------------------------------------------------//
-  //----------------------------------//pedestrian crossing
-/*  if (Serial2.available()) {
-    if (Serial2.read() == 5 ) {
-      analogWrite(pwm, slowSpeed);
-    }
-  }
 
-  //----------------------------------//
-  //------------------------------------------------------------// stop sigh
+  analogWrite(pwm, speed1);  //start the engine
+
+
+  //------------------------------------------------------------------------// check signals
   if (Serial2.available()) {
-    if (Serial2.read() == 6) {
+    irda = Serial2.read();
+    if (irda == 0 || irda == 1 || irda == 4) {  //if red,red+yellow,yellow
       analogWrite(pwm, 0);
-      delay(5000);
-
     }
-  }*/
-  //------------------------------------------------------------//
-  //----------------------------------//distance attributive
-  /*  d1 = 5222 / (analogRead(A6) - 13);                     //probably it changes values into cm
-    d2 = 5222 / (analogRead(A7) - 13);
-    d3 = 5222 / (analogRead(A8) - 13);
-      if ((d1 <= distanceEdge) || (d2 <= distanceEdge) || (d3 <= distanceEdge)) {
-        while ((1 <= distanceEdge) || (d2 <= distanceEdge) || (d3 <= distanceEdge)) {
-
-          d1 = 5222 / (analogRead(A6) - 13);
-          d2 = 5222 / (analogRead(A7) - 13);
-          d3 = 5222 / (analogRead(A8) - 13);
-          analogWrite(pwm, 0);
-        //  Serial.println(d1);
-       //   Serial.print(" ");
-       //   Serial.println(d2);
-       //   Serial.print(" ");
-       //   Serial.println(d3);
+  }
 
 
-        }
+ /*   
+      //----------------------------------//pedestrian crossing
+    if (Serial2.available()) {
+      if (Serial2.read() == 5 ) {
+        analogWrite(pwm, slowSpeed);
+      }
+    }
 
+    //----------------------------------//
+*/
 
-      }*/
- /* d1 = 5222 / (analogRead(A6) - 13);
+/*    
+    //----------------------------------// stop sigh
+    if (Serial2.available()) {
+      if (Serial2.read() == 6) {
+        analogWrite(pwm, 0);
+        delay(5000);
+
+      }
+    }
+ */
+  //------------------------------------//
+  //---------------------------------------------------------------------//distance attributive
+  d1 = 5222 / (analogRead(A6) - 13);    //changes values into cm
   d2 = 5222 / (analogRead(A7) - 13);
-//  if (( d2 <= distanceEdge) || (d2 <= distanceEdge)) {
-    while ((d2 <= distanceEdge) || (d2 <= distanceEdge)) {
-      d2 = 5222 / (analogRead(A7) - 13);
+  d3 = 5222 / (analogRead(A8) - 13);
+  
+  if ((d1 <= distanceEdge) || (d2 <= distanceEdge) || (d3 <= distanceEdge)) {     // if there're barriers
+    while ((d1 <= distanceEdge) || (d2 <= distanceEdge) || (d3 <= distanceEdge)) {
+      
       d1 = 5222 / (analogRead(A6) - 13);
+      d2 = 5222 / (analogRead(A7) - 13);
+      d3 = 5222 / (analogRead(A8) - 13);
       analogWrite(pwm, 0);
-   //   Serial.println(d2);
-//    }
-  }*/
+      /*Serial.println(d1);
+        Serial.print(" ");
+        Serial.println(d2);
+        Serial.print(" ");
+        Serial.println(d3);
+      */
+    }
+}
 
-  //----------------------------------//
-  //------------------------------------------------------------//irda things
-
-  /*  irda = Serial2.read();
-     if (irda == 0 || irda == 1 || irda == 4) { //if red,red+yellow,yellow
-      speed1 = speedBeforCrossing;
-      //program should wait for a stopline more intently. It helps to differ a crossing and a stopline.
-     }
-  */
-  //------------------------------------------------------------//
   //---------------------------------------------//stopline + irda
-  /*if (digitalRead(40) == 1) {
-    speed1 = 0;
-    Serial.print("STOP");
-
-    }*/
- /* if (Serial3.available()) {
-    int read1 = Serial3.read();
-    /* Serial.print(read1);
-      Serial.print(" \n");*/
+  
+  /* if (Serial3.available()) {
+     int read1 = Serial3.read();
+     /* Serial.print(read1);
+       Serial.print(" \n");*/
   /*  if (read1 == 1) {
       if (Serial2.available()) {
         irda = Serial2.read();
@@ -157,16 +140,16 @@ void loop() {
 
       }
     }
-  }*/
+    }*/
 
+/*
   irda = Serial2.read();
-  if (irda == 0 || irda == 1 || irda == 4) { //if red,red+yellow,yellow
+  if (irda == 0 || irda == 1 || irda == 4) {
     speed1 = speedBeforCrossing;
-
-  }
-
+ }
+*/
   //------------------------------------------------------------//
-  //---------------------------------------------//stopline
+  //-----------------------------//stopline
   if (Serial3.available()) {
     stopline = Serial3.read();
     Serial.println(stopline);
@@ -176,8 +159,6 @@ void loop() {
     }
   }
 
-
-  //-----------------------------------------------//
   //-----------------------------------------------------------//read from camera
   if (Serial1.available()) {
     // incomingByte = Serial1.read();
@@ -186,41 +167,24 @@ void loop() {
 
     //    Serial.print(" ");
     if (angle > 128)
-        {
-          angle -= 256;
-        }
-        
-    //angle *= -1;
-   // if ((angle < 12) && (angle > -12))
- 
+    {
+      angle -= 256;
+    }
+
+    anngle*=2;     // mechanic coefficient
+
     angle = angle + 90;
     Serial.println(angle);
     //     Serial.print(angle);
     //   Serial.print(" ");
-   /* if (angle > 156)
+   
+    servo.write(angle);
+    
 
-    {
-      servo.write((angle * 1.2) + 90);
-    }*/
 
-/*else
-      servo.write((angle * 2.8) + 90);*/
-      servo.write(angle);
-    // angle = (angle * 1.7) + 90;
-  //  Serial.println((angle * 2.8) + 90);
-    //  Serial.println(angle);
-    //  delay(10);
-    //servo.write((angle * 1.5) + 90);
-
-       
 
     //  delay(10);
-    //  Serial.print(angle);
     //Serial.print(incomingByte);
-
-    //  Serial.print(" \n");
-
-    //    Serial.print(" \n");
 
   }
   //----------------------------------------------------------//

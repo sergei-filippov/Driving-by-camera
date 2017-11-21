f8ce3bac4709b5f0d41c5ad9c160cd9c7b9ab160
sergei <sergei-filippov2001@protonmail.com>
2017-11-21 17:59:07 +0300

    1

--- 16.06.2017 Все о камере/rbtrffc_israel_2017_v/distanceRead_ex1/distanceRead_ex1.ino
+++ 16.06.2017 Все о камере/rbtrffc_israel_2017_v/distanceRead_ex1/distanceRead_ex1.ino
@@ -1,17 +1,30 @@
-int d, rowA6; // расстояние и значения с датчика
+int d, row13,row14,row15,row12; // расстояние и значения с датчика
 void setup() {
   Serial.begin(9600);
  // pinMode(A10,INPUT);
+ pinMode(A12,INPUT);
   pinMode(A14,INPUT);
+  pinMode(A15,INPUT);
+  pinMode(A13,INPUT);
 
 }
 
 void loop() {
-  rowA6=analogRead(A14);
- Serial.println(rowA6);
+  row12=analogRead(A12);
+  row14=analogRead(A14);
+  row15=analogRead(A15);
+  row13=analogRead(A13);
+ //Serial.println(rowA6);
+ // Serial.print(" ");
+
+    Serial.print(5222/(row12-13));
   Serial.print(" ");
-d=5222/(rowA6-13);
-  Serial.println(d);
+  Serial.print(5222/(row13-13));
+  Serial.print(" ");
+  Serial.print(5222/(row14-13));
+  Serial.print(" ");
+  Serial.print(5222/(row15-13));
+  Serial.println(" ");
   delay(200);
 
 }
